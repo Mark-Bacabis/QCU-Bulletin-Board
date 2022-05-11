92c253c066fa8aaebc0f02b372df23f000e73bd7
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-05-11 19:49:19 +0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -12,7 +12,7 @@
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/> <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" /> 
 
 
-   <title> QCU Bulletin Board Admin Dashboard </title>
+   <title> QCU Bulletin Board Dashboard </title>
 </head>
 
 <body>
