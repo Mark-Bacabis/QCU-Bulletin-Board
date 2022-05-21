ba82f2114d8ec69ba85ab7dc8520315b2cb836b5
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-05-21 15:45:43 +0800

    upd

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,13 @@
 <?php
    session_start();
-  
+   include "../process/function.php";
    unset($_SESSION['empID']);
+         
+   $schoolID = $_SESSION['empID'];  
+   $activity = "Logged in";  
+   $date = date('Y-m-d');
+   $time = date('H:i:s A');     
+
+   actLog($schoolID, $activity, $date, $time);
    header("location: ./index.php");
 ?>
