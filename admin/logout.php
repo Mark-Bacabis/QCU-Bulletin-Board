122c2b1d3f5bdbf695604b23c986e7a0f6d956ac
Mark-Bacabis <mark.melvin.bacabis@gmail.com>
2022-05-21 15:48:34 +0800

    upd

--- admin/logout.php
+++ admin/logout.php
@@ -1,13 +1,13 @@
 <?php
    session_start();
    include "../process/function.php";
-   unset($_SESSION['empID']);
-         
    $schoolID = $_SESSION['empID'];  
-   $activity = "Logged in";  
+   $activity = "Logged out";  
    $date = date('Y-m-d');
    $time = date('H:i:s A');     
 
    actLog($schoolID, $activity, $date, $time);
+
+   unset($_SESSION['empID']);
    header("location: ./index.php");
 ?>
--- student/logout.php
+++ student/logout.php
@@ -1,5 +1,12 @@
 <?php 
   session_start();
+  include "../process/function.php";
+  $schoolID = $_SESSION['userid'];  
+  $activity = "Logged out";  
+  $date = date('Y-m-d');
+  $time = date('H:i:s A');     
+
+  actLog($schoolID, $activity, $date, $time);
   unset($_SESSION['userid']);
   header("location: ./login.php");
 ?>
